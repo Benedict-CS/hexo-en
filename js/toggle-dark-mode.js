f2d7e73de047b40f0af062ce69e780ecbae07861
Benedict-CS <benedicttiong.cs@gmail.com>
2024-07-02 01:25:47 +0800

    Site updated: 2024-07-02 01:25:45

--- js/toggle-dark-mode.js
+++ js/toggle-dark-mode.js
@@ -1,56 +1,66 @@
 document.addEventListener('DOMContentLoaded', () => {
+  if (typeof Darkmode === 'undefined') {
+    return;
+  }
+
+  const options = {
+    bottom: '64px',
+    right: '32px',
+    left: 'unset',
+    time: '0.3s',
+    mixColor: 'transparent',
+    backgroundColor: 'transparent',
+    buttonColorDark: '#100f2c',
+    buttonColorLight: '#fff',
+    isActivated: true,
+    saveInCookies: false,
+    autoMatchOsTheme: false
+  };
   
-    // 检查Darkmode.js是否已加载
-    if (typeof Darkmode === 'undefined') {
-      return;
-    }
+  const darkmode = new Darkmode(options);
   
-    // 初始化Darkmode.js选项
-    const options = {
-      bottom: '64px',
-      right: '32px',
-      left: 'unset',
-      time: '0.3s',
-      mixColor: 'transparent',
-      backgroundColor: 'transparent',
-      buttonColorDark: '#100f2c',
-      buttonColorLight: '#fff',
-      isActivated: true,
-      saveInCookies: false,
-      autoMatchOsTheme: false
-    };
+  window.toggleDarkMode = function () {
+    darkmode.toggle();
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const targetTheme = currentTheme === "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("data-theme", targetTheme);
+    localStorage.setItem('theme', targetTheme);
   
-    // 手动切换模式
-    window.toggleDarkMode = function () {
-      const darkmode = new Darkmode(options);
-      darkmode.toggle();
-      const currentTheme = document.documentElement.getAttribute("data-theme");
-      const targetTheme = currentTheme === "dark" ? "light" : "dark";
-      document.documentElement.setAttribute("data-theme", targetTheme);
-      localStorage.setItem('theme', targetTheme); // 保存到本地存储
-  
-      // 切换图标
-      const themeIcon = document.getElementById('theme-icon');
-      if (targetTheme === "dark") {
-        themeIcon.classList.remove('fa-moon');
-        themeIcon.classList.add('fa-sun');
-      } else {
-        themeIcon.classList.remove('fa-sun');
-        themeIcon.classList.add('fa-moon');
-      }
-    };
-  
-    // 在页面加载时应用保存的主题和图标
-    const savedTheme = localStorage.getItem('theme') || 'light';
-    document.documentElement.setAttribute('data-theme', savedTheme);
-  
+    const githubCommentFrame = document.querySelector('.utterances-frame');
+
     const themeIcon = document.getElementById('theme-icon');
-    if (savedTheme === "dark") {
+    if (targetTheme === "dark") {
       themeIcon.classList.remove('fa-moon');
       themeIcon.classList.add('fa-sun');
+      
+      if (githubCommentFrame) {
+        githubCommentFrame.contentWindow.postMessage({
+          type: 'set-theme',
+          theme: 'github-dark'
+        }, 'https://utteranc.es');
+      }
     } else {
       themeIcon.classList.remove('fa-sun');
       themeIcon.classList.add('fa-moon');
+      
+      if (githubCommentFrame) {
+        githubCommentFrame.contentWindow.postMessage({
+          type: 'set-theme',
+          theme: 'github-light'
+        }, 'https://utteranc.es');
+      }
     }
-  });
+  };
   
+  const savedTheme = localStorage.getItem('theme') || 'light';
+  document.documentElement.setAttribute('data-theme', savedTheme);
+  
+  const themeIcon = document.getElementById('theme-icon');
+  if (savedTheme === "dark") {
+    themeIcon.classList.remove('fa-moon');
+    themeIcon.classList.add('fa-sun');
+  } else {
+    themeIcon.classList.remove('fa-sun');
+    themeIcon.classList.add('fa-moon');
+  }
+});
